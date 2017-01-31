fed15d1e41034a97799478dc0a4af2c89a3c4fa6
FromDarkHell <treepuncher0705@gmail.com>
2017-01-30 20:10:26 -0600

    Add SkiFree, Rodents, PipeDream Base Images and Button Click Events

---
+++ Windows Entertainment Recoded/Windows Entertainment Recoded/Menu.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Windows_Entertainment_Recoded
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SkiFreeBox = new System.Windows.Forms.PictureBox();
+            this.SkiFreeButton = new System.Windows.Forms.Button();
+            this.RodentsBox1 = new System.Windows.Forms.PictureBox();
+            this.Rodents = new System.Windows.Forms.Button();
+            this.SkiFreeBox2 = new System.Windows.Forms.PictureBox();
+            this.RodentsBox2 = new System.Windows.Forms.PictureBox();
+            this.RodentsRemade = new System.Windows.Forms.Button();
+            this.DreamBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DreamBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SkiFreeButton2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SkiFreeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RodentsBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SkiFreeBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RodentsBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DreamBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DreamBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SkiFreeBox
+            // 
+            this.SkiFreeBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.SkiFreeBox.Location = new System.Drawing.Point(35, 1);
+            this.SkiFreeBox.Name = "SkiFreeBox";
+            this.SkiFreeBox.Size = new System.Drawing.Size(47, 39);
+            this.SkiFreeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SkiFreeBox.TabIndex = 0;
+            this.SkiFreeBox.TabStop = false;
+            // 
+            // SkiFreeButton
+            // 
+            this.SkiFreeButton.Location = new System.Drawing.Point(-2, 38);
+            this.SkiFreeButton.Name = "SkiFreeButton";
+            this.SkiFreeButton.Size = new System.Drawing.Size(141, 22);
+            this.SkiFreeButton.TabIndex = 1;
+            this.SkiFreeButton.Text = "SkiFree Original";
+            this.SkiFreeButton.UseVisualStyleBackColor = true;
+            this.SkiFreeButton.Click += new System.EventHandler(this.SkiFreeButton_Click);
+            // 
+            // RodentsBox1
+            // 
+            this.RodentsBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.RodentsBox1.Location = new System.Drawing.Point(179, 1);
+            this.RodentsBox1.Name = "RodentsBox1";
+            this.RodentsBox1.Size = new System.Drawing.Size(47, 39);
+            this.RodentsBox1.TabIndex = 2;
+            this.RodentsBox1.TabStop = false;
+            // 
+            // Rodents
+            // 
+            this.Rodents.Location = new System.Drawing.Point(147, 38);
+            this.Rodents.Name = "Rodents";
+            this.Rodents.Size = new System.Drawing.Size(110, 22);
+            this.Rodents.TabIndex = 3;
+            this.Rodents.Text = "Rodents Revenge";
+            this.Rodents.UseVisualStyleBackColor = true;
+            this.Rodents.Click += new System.EventHandler(this.Rodents_Click);
+            // 
+            // SkiFreeBox2
+            // 
+            this.SkiFreeBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.SkiFreeBox2.Location = new System.Drawing.Point(35, 66);
+            this.SkiFreeBox2.Name = "SkiFreeBox2";
+            this.SkiFreeBox2.Size = new System.Drawing.Size(47, 39);
+            this.SkiFreeBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SkiFreeBox2.TabIndex = 4;
+            this.SkiFreeBox2.TabStop = false;
+            // 
+            // RodentsBox2
+            // 
+            this.RodentsBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.RodentsBox2.Location = new System.Drawing.Point(179, 66);
+            this.RodentsBox2.Name = "RodentsBox2";
+            this.RodentsBox2.Size = new System.Drawing.Size(47, 39);
+            this.RodentsBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RodentsBox2.TabIndex = 6;
+            this.RodentsBox2.TabStop = false;
+            // 
+            // RodentsRemade
+            // 
+            this.RodentsRemade.Location = new System.Drawing.Point(147, 111);
+            this.RodentsRemade.Name = "RodentsRemade";
+            this.RodentsRemade.Size = new System.Drawing.Size(110, 22);
+            this.RodentsRemade.TabIndex = 7;
+            this.RodentsRemade.Text = "Rodents Remade";
+            this.RodentsRemade.UseVisualStyleBackColor = true;
+            this.RodentsRemade.Click += new System.EventHandler(this.RodentsRemade_Click);
+            // 
+            // DreamBox
+            // 
+            this.DreamBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.DreamBox.Location = new System.Drawing.Point(294, 1);
+            this.DreamBox.Name = "DreamBox";
+            this.DreamBox.Size = new System.Drawing.Size(47, 39);
+            this.DreamBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DreamBox.TabIndex = 8;
+            this.DreamBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 22);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "PipeDream";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DreamBox2
+            // 
+            this.DreamBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.DreamBox2.Location = new System.Drawing.Point(294, 66);
+            this.DreamBox2.Name = "DreamBox2";
+            this.DreamBox2.Size = new System.Drawing.Size(47, 39);
+            this.DreamBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DreamBox2.TabIndex = 10;
+            this.DreamBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 22);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "PipeDream Remade";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // SkiFreeButton2
+            // 
+            this.SkiFreeButton2.Location = new System.Drawing.Point(-2, 111);
+            this.SkiFreeButton2.Name = "SkiFreeButton2";
+            this.SkiFreeButton2.Size = new System.Drawing.Size(141, 22);
+            this.SkiFreeButton2.TabIndex = 12;
+            this.SkiFreeButton2.Text = "SkiFree Remade";
+            this.SkiFreeButton2.UseVisualStyleBackColor = true;
+            this.SkiFreeButton2.Click += new System.EventHandler(this.SkiFreeButton2_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(551, 302);
+            this.Controls.Add(this.SkiFreeButton2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DreamBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DreamBox);
+            this.Controls.Add(this.RodentsRemade);
+            this.Controls.Add(this.RodentsBox2);
+            this.Controls.Add(this.SkiFreeBox2);
+            this.Controls.Add(this.Rodents);
+            this.Controls.Add(this.RodentsBox1);
+            this.Controls.Add(this.SkiFreeButton);
+            this.Controls.Add(this.SkiFreeBox);
+            this.Name = "Menu";
+            this.Text = "Best of Windows Entertainment Remade";
+            ((System.ComponentModel.ISupportInitialize)(this.SkiFreeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RodentsBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SkiFreeBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RodentsBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DreamBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DreamBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SkiFreeBox;
+        private System.Windows.Forms.Button SkiFreeButton;
+        private System.Windows.Forms.PictureBox RodentsBox1;
+        private System.Windows.Forms.Button Rodents;
+        private System.Windows.Forms.PictureBox SkiFreeBox2;
+        private System.Windows.Forms.PictureBox RodentsBox2;
+        private System.Windows.Forms.Button RodentsRemade;
+        private System.Windows.Forms.PictureBox DreamBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox DreamBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button SkiFreeButton2;
+    }
+}
+
---
+++ Windows Entertainment Recoded/Windows Entertainment Recoded/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Windows_Entertainment_Recoded
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            string StringFree = Directory.GetCurrentDirectory() + "\\SkiFreesources\\SkiFree.gif";
+            string StringRodents = Directory.GetCurrentDirectory() + "\\RodentsResources\\RodentsRevenge.jpg";
+            string StringDream = Directory.GetCurrentDirectory() + "\\PipeDreasources\\PipeDream.jpg";
+            Image ImageFree = Image.FromFile(StringFree);
+            Image ImageRodents = Image.FromFile(StringRodents);
+            Image ImageDream = Image.FromFile(StringDream);
+            SkiFreeBox.BackgroundImage = ImageFree;
+            SkiFreeBox2.BackgroundImage = ImageFree;
+            RodentsBox1.BackgroundImage = ImageRodents;
+            RodentsBox2.BackgroundImage = ImageRodents;
+            DreamBox.BackgroundImage = ImageDream;
+            DreamBox2.BackgroundImage = ImageDream;
+        }
+        private void SkiFreeButton_Click(object sender, EventArgs e) {
+        }
+        private void SkiFreeButton2_Click(object sender, EventArgs e) {
+        }
+        private void Rodents_Click(object sender, EventArgs e) {
+        }
+        private void RodentsRemade_Click(object sender, EventArgs e) {
+        }
+    }
+}
